9f04007d2bf2b28054bdfcac6bf55366006eec5f
Evgeniy <egeniygal@gmail.com>
2024-04-22 20:46:50 +0300

    All done

---
+++ contacts.js
@@ -0,0 +1,39 @@
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+import { v4 as uuidv4 } from "uuid";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+export const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
+
+export const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+  return result || null;
+};
+
+export const removeContacts = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+};
+
+export const addContacts = async (body) => {
+  const contacts = await listContacts();
+  const newContact = { ...body, id: uuidv4() };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
---
+++ index.js
@@ -0,0 +1,49 @@
+import {
+  addContacts,
+  getContactById,
+  listContacts,
+  removeContacts,
+} from "./contacts.js";
+import { program } from "commander";
+
+console.log(listContacts());
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contacts = await listContacts();
+      console.table(contacts);
+      break;
+
+    case "get":
+      const contact = await getContactById(id);
+      console.log(contact ?? "Contact not found");
+      break;
+
+    case "add":
+      const newContact = await addContacts({ name, email, phone });
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const removedContact = await removeContacts(id);
+      console.log(removedContact ?? "Contact not found");
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
